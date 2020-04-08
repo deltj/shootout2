648ecd335bebcc181fdde3e498aeccbdade22759
Ted <deltj@outlook.com>
2020-04-08 17:58:10 -0400

    counting missed packets

--- shootout2.cpp
+++ shootout2.cpp
@@ -9,14 +9,15 @@
 #include <iostream>
 #include <map>
 #include <mutex>
-#include <set>
 #include <queue>
+#include <set>
 #include <thread>
 
 extern "C" {
+#include <getopt.h>
+#include <pcap.h>
 #include <signal.h>
 #include <unistd.h>
-#include <pcap.h>
 }
 
 #include "Packet.h"
@@ -32,9 +33,21 @@ std::condition_variable packetQueueCv;
 std::set<shootout::PacketHash> allHashes;
 std::mutex allHashesMutex;
 
-//  Hashes by interface
-std::map<int, std::set<shootout::PacketHash> > hashesByInterface;
-std::mutex hashesByInterfaceMutex;
+//  Observed hashes (hits) by interface
+std::map<int, std::set<shootout::PacketHash> > hitByInterface;
+std::mutex hitByInterfaceMutex;
+
+//  Missed hashes by interface
+std::map<int, std::set<shootout::PacketHash> > missByInterface;
+
+//  This structure stores interface information to be passed between the main
+//  thread and a worker thread
+struct NL80211Interface
+{
+    std::string name;
+    int ifindex;
+    std::thread thread;
+};
 
 void sighandler(int signum)
 {
@@ -48,12 +61,19 @@ void sighandler(int signum)
  * This thread function captures packets from libpcap and adds them to the 
  * packet set.
  */
-void captureThreadFn()
+void captureThreadFn(std::shared_ptr<NL80211Interface> ifc)
 {
     char errbuf[PCAP_ERRBUF_SIZE];
 
-    printf("pcap_open_live\n");
-    pcap_t *p = pcap_open_live("wlp7s0", 800, 1, 20, errbuf);
+    if(ifc == nullptr)
+    {
+        fprintf(stderr, "Capture thread argument was null\n");
+        return;
+    }
+
+    printf("Starting capture thread for %d (%s)\n", ifc->ifindex, ifc->name.c_str());
+
+    pcap_t *p = pcap_open_live(ifc->name.c_str(), 800, 1, 20, errbuf);
     if(p == NULL)
     {
         fprintf(stderr, "pcap_open_live failed: %s\n", errbuf);
@@ -79,9 +99,11 @@ void captureThreadFn()
         //  Make a new Packet object for the received packet
         shootout::Packet p;
         p.setData(data, hdr->len);
+        p.ifindex = ifc->ifindex;
 
         //  Lock the packetQueue mutex and push the new Packet
         {
+            //TODO: add a lock timeout
             std::lock_guard<std::mutex> lk(packetQueueMutex);
             packetQueue.push(p);
         }
@@ -98,6 +120,7 @@ void statThreadFn()
     while(!quit)
     {
         //  Wait for notification that there's a new Packet to process
+        //TODO: add a lock timeout
         std::unique_lock<std::mutex> lk(packetQueueMutex);
         packetQueueCv.wait(lk);
 
@@ -113,47 +136,113 @@ void statThreadFn()
             allHashesMutex.unlock();
 
             //  Take note of which interface has observed the hash
-            hashesByInterfaceMutex.lock();
-            hashesByInterface[p.ifindex].insert(p.hash);
-            hashesByInterfaceMutex.unlock();
+            hitByInterfaceMutex.lock();
+            hitByInterface[p.ifindex].insert(p.hash);
+            hitByInterfaceMutex.unlock();
         }
 
         lk.unlock();
     }
 }
 
+bool interfaceHit(int ifindex, shootout::PacketHash hash)
+{
+    std::unique_lock<std::mutex> lk(hitByInterfaceMutex);
+    std::set<shootout::PacketHash>::const_iterator f = hitByInterface[ifindex].find(hash);
+    return f != hitByInterface[ifindex].end();
+}
+
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
     signal(SIGINT, sighandler);
 
-    printf("starting threads\n");
-    std::thread captureThread(captureThreadFn);
+    std::vector<std::shared_ptr<NL80211Interface> > interfaces;
+
+    //  Configure program options
+    struct option long_opts[] =
+    {
+        { "interface", required_argument, 0, 'i' },
+        { 0, 0, 0, 0 }
+    };
+
+    const char *optstr = "i:";
+    int opt;
+    while((opt = getopt_long(argc, argv, optstr, long_opts, &optind)) != -1)
+    {
+        switch(opt)
+        {
+        case 'i':
+            {
+                printf("Using interface %s\n", optarg);
+
+                //  Set up a new interface
+                std::shared_ptr<NL80211Interface> ifc =
+                        std::make_shared<NL80211Interface>();
+                ifc->name = std::string(optarg);
+                ifc->ifindex = rand() % 100;
+
+                interfaces.push_back(ifc);
+            }
+            break;
+            
+        default:
+            fprintf(stderr, "Unrecognized argument %c\n", opt);
+            break;
+        }
+    }
+
+    //TODO: Configure interfaces with netlink
+
+    //  Start a capture thread for each interface
+    for(std::vector<std::shared_ptr<NL80211Interface> >::iterator it = interfaces.begin();
+            it != interfaces.end(); ++it)
+    {
+        (*it)->thread = std::thread(captureThreadFn, *it);
+    }
+
     std::thread statThread(statThreadFn);
 
     while(!quit)
     {
         printf("\n");
 
-        packetQueueMutex.lock();
-        printf("Packets waiting to be processed: %lu\n", packetQueue.size());
-        packetQueueMutex.unlock();
+        //packetQueueMutex.lock();
+        //printf("Packets waiting to be processed: %lu\n", packetQueue.size());
+        //packetQueueMutex.unlock();
 
         allHashesMutex.lock();
-        printf("All hashes: %lu\n", allHashes.size());
+        size_t totalPackets = allHashes.size();
         allHashesMutex.unlock();
 
-        hashesByInterfaceMutex.lock();
-        std::map<int, std::set<shootout::PacketHash> >::const_iterator it;
-        for(it = hashesByInterface.begin(); it != hashesByInterface.end(); ++it)
+        printf("Total packets: %lu\n", totalPackets);
+
+        for(std::vector<std::shared_ptr<NL80211Interface> >::iterator iit = interfaces.begin();
+                iit != interfaces.end(); ++iit)
         {
-            printf("Hashes for interface %d: %lu\n", it->first, it->second.size());
+            //  Find missed packets for this interface
+            //  Note: This loop will not scale well...  find a better solution
+            allHashesMutex.lock();
+            for(std::set<shootout::PacketHash>::const_iterator hit = allHashes.begin();
+                    hit != allHashes.end(); ++hit)
+            {
+                if(!interfaceHit((*iit)->ifindex, *hit))
+                {
+                    //  The interface (iit) missed this hash (hit)
+                    missByInterface[(*iit)->ifindex].insert(*hit);
+                }
+            }
+            allHashesMutex.unlock();
+
+            printf("Missed packets for %s: %lu (%0.1f)\n", (*iit)->name.c_str(),
+                    missByInterface[(*iit)->ifindex].size(),
+                    missByInterface[(*iit)->ifindex].size() / (double)totalPackets * 100);
         }
-        hashesByInterfaceMutex.unlock();
 
         sleep(1);
     }
 
-    captureThread.join();
+    //TODO: join all the capture threads
     statThread.join();
 
     return EXIT_SUCCESS;
